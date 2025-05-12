ca4f26132e5192f5737c0de663c52ee1d57fbb13
Demetrio Esteban Alférez <33077142+demstalferez@users.noreply.github.com>
2025-05-13 02:35:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly_express as px
+import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
